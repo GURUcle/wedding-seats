0165c1c1653dfd2b8777c43589157aa4606e49aa
Optiflex Optiflex <Optiflex Optiflex>
2016-08-25 17:43:52 +0200

    Update

--- cloud/models/guest.js
+++ cloud/models/guest.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Guest', new mongoose.Schema({
+    name: { type: String, default: "" },
+    surname: { type: String, default: "" },
+    contact: { type: String, default: "" },
+    table: { type: String, default: "" },
+    seat: { type: String, default: "" },
     created_on: { type: Date, default: Date.now }
 }));
