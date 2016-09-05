252a1919f9504a35bfcf30734353c0f803983188
Optiflex Optiflex <Optiflex Optiflex>
2016-09-05 09:22:12 +0200

    Fixed Changes

--- cloud/controllers/ussd.js
+++ cloud/controllers/ussd.js
@@ -9,58 +9,47 @@ module.exports = function (app) {
 
     app.get('/api/ussd/home', function (req, res) {
 
+        var text = "";
+        var tables = [];
+
         var findObject = {
             contact: req.query.ussd_msisdn
         };
 
-        var text = "";
-
         CRUD().findAll(Guest, findObject, 'asc', 1000, 0, function (data) {
-            if (data) {
-                if (data.length > 1) {
+            if (data && data.length > 0) {
 
-                    text = "";
+                text = "";
+                var cursor = 1;
 
-                    for (var i = 0; i < data.length; i++) {
-                        text += "Seat " + data[i].seat + ": " + data[i].name + " " + data[i].surname + "\n";
-                    }
-
-                    res.type('text/plain');
-                    res.send(util.format(nodes.Home.Text, data[0].table, text));
-
-                    request("http://api.panaceamobile.com/json?action=message_send&username=CrunchingCode&password=nchongin00&to=" + req.query.ussd_msisdn + "&text=" + util.format(nodes.Home.Text, data[0].table, text) + "&from=27726422105&auto_detect_encoding=1", function (error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            console.log(response);
-                        } else {
-                            console.log(response);
-                        }
-                    });
-
-                } else if (data.length > 0) {
-
-                    text = "";
-
-                    text += "Seat " + data[0].seat + ": " + data[0].name + " " + data[0].surname + "\n";
-
-                    res.type('text/plain');
-                    res.send(util.format(nodes.Home.Text, data[0].table, text));
-
-                    request("http://api.panaceamobile.com/json?action=message_send&username=CrunchingCode&password=nchongin00&to=" + req.query.ussd_msisdn + "&text=" + util.format(nodes.Home.Text, data[0].table, text) + "&from=27726422105&auto_detect_encoding=1", function (error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            console.log(response);
-                        } else {
-                            console.log(response);
-                        }
-                    });
-
-                } else {
-                    res.type('text/plain');
-                    res.send(util.format(nodes.Home.Error, req.query.ussd_msisdn));
+                for (var i = 0; i < data.length; i++) {
+                    tables.push(data[i].table);
                 }
+
+                var unique = tables.filter(function (elem, index, self) {
+                    return index == self.indexOf(elem);
+                });
+
+                for (var x = 0; x < unique.length; x++) {
+                    text += cursor + ". Table " + data[x].table + "\n";
+                    cursor++;
+                }
+
+                res.type('text/plain');
+                res.send(util.format(nodes.Home.Text, text));
+
+                request("http://api.panaceamobile.com/json?action=message_send&username=CrunchingCode&password=nchongin00&to=" + req.query.ussd_msisdn + "&text=" + util.format(nodes.Home.Text, data[0].table, text) + "&from=27726422105&auto_detect_encoding=1", function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log(response);
+                    } else {
+                        console.log(response);
+                    }
+                });
 
             } else {
                 res.type('text/plain');
                 res.send(util.format(nodes.Home.Error, req.query.ussd_msisdn));
+
             }
         }, function (error) {
             res.type('text/plain');
@@ -73,9 +62,42 @@ module.exports = function (app) {
 
         var table = req.query.ussd_response_WeddingSeatsChairs;
         var text = "";
+        var tables = [];
 
-        res.type('text/plain');
-        res.send(util.format(nodes.Chairs.Error, req.query.ussd_msisdn));
+        var findObject = {
+            contact: req.query.ussd_msisdn
+        };
+
+        CRUD().findAll(Guest, findObject, 'asc', 1000, 0, function (data) {
+            if (data && data.length > 0) {
+
+                text = "";
+                var cursor = 1;
+                var index = "" + table - 1;
+
+                for (var i = 0; i < data.length; i++) {
+                    tables.push(data[i].table);
+                }
+
+                var unique = tables.filter(function (elem, index, self) {
+                    return index == self.indexOf(elem);
+                });
+
+                for (var x = 0; x < data.length; x++) {
+                    if (data[x].table === unique[index]) {
+                        text += cursor + ". Seat " + data[x].seat + "\n";
+                        cursor++;
+                    }
+                }
+
+                res.type('text/plain');
+                res.send(util.format(nodes.Chairs.Text, data[0].table, text));
+
+            }
+        }, function (error) {
+            res.type('text/plain');
+            res.send(util.format(nodes.Chairs.Error, req.query.ussd_msisdn));
+        });
     });
 
     app.get('/api/ussd/exit', function (req, res) {
